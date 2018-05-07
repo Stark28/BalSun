8d0305a6ed4807789587c7fe61a546acfae50f74
Stark28 <31844579+Stark28@users.noreply.github.com>
2018-05-07 17:56:12 +0200

    lalala
    Co-Authored-By: stark28 <stark28@users.noreply.github.com>
    Co-Authored-By: balajinagarajkumar <balajinagarajkumar@users.noreply.github.com>

--- trial1/src/trial1/Amain.java
+++ trial1/src/trial1/Amain.java
@@ -9,7 +9,7 @@ import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
-import Project1Pack.BaseVoltClass;
+//import Project1Pack.BaseVoltClass;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
@@ -29,13 +29,21 @@ public class Amain {
 		
 		//Extract required data from EQ file
 		ArrayList BaseVoltageList = new ArrayList<String>();
-		NodeList basevoltlist = doc1.getElementsByTagName("cim:BaseVoltage");
+		//NodeList basevoltlist = doc1.getElementsByTagName("cim:BaseVoltage");
+		
+		ArrayList SubstationList = new ArrayList<String>();
+		//NodeList substationlist = doc1.getElementsByTagName("cim:Substation");
+		
 		
 		// Base Voltage information
 			BaseVoltClass basevolt = new BaseVoltClass();
 			basevolt.basevoltfn(doc1, BaseVoltageList);
 			System.out.println("List of Base Voltage : " + BaseVoltageList);
 		
+		// Substation information
+			SubstationClass subs = new SubstationClass();
+			subs.substationfn(doc1, SubstationList);
+			System.out.println("List of Substation : " + SubstationList);
 		
 		
 	}
@@ -44,6 +52,4 @@ public class Amain {
 		}
 	}
 }
-	//public String getbasevoltlist() {
-	//	return  BaseVol;
-	//}
+
--- trial1/src/trial1/SubstationClass.java
+++ trial1/src/trial1/SubstationClass.java
@@ -1,5 +1,41 @@
 package trial1;
 
-public class SubstationClass {
 
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class SubstationClass{
+	
+public static ArrayList substationfn(Document doc1, ArrayList SubstationList)
+{
+	NodeList substationlist = doc1.getElementsByTagName("cim:Substation");
+	System.out.println("Substation : ");
+	String rdfID = null;
+	String name; 
+	String regionID;
+	
+	for (int i = 0; i<substationlist.getLength(); i++) 
+	{
+	Node Subs = substationlist.item(i);
+	   
+	Element element = (Element) Subs;
+	rdfID = element.getAttribute("rdf:ID");
+	name = element.getElementsByTagName("cim:IdentifiedObject.name").item(0).getTextContent();
+	regionID = element.getElementsByTagName("cim:Substation.Region").item(0).getAttributes().item(0).getTextContent().replaceAll("#","");
+	
+	System.out.println("Reference ID : " + rdfID);
+    System.out.println("Name : " + name);
+    System.out.println("RegionID : " + regionID);
+    
+    SubstationList.add(rdfID);
+    SubstationList.add(name);
+    SubstationList.add(regionID);
+    
+}
+	return(SubstationList);
+}	 
 }
